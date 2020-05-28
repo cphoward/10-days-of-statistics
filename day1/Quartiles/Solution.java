67d950d7a34da6baf3db01656397294ab0fd1f41
Casey Howard <casey.p.howard@outlook.com>
2020-05-28 16:28:05 -0400

    moved and adjusted files, completed day1/Quartiles

---
+++ day1/Quartiles/Solution.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Solution {
+
+  public static int median(final ArrayList<Integer> al) {
+    final int n = al.size();
+    if (n % 2 == 1) {
+        return al.get(n/2);
+    } else {
+        final int left = n/2-1;
+        final int right = n/2;
+        return ((al.get(left))+al.get(right))/2;
+    }
+  }
+
+  public static void main(final String[] args) {
+    final Scanner scan = new Scanner(System.in);
+    final int n = scan.nextInt();
+    final ArrayList<Integer> al = new ArrayList<>();
+    for (int i = 0; i < n; i++) {
+      al.add(scan.nextInt());
+    }
+    scan.close();
+    Collections.sort(al);
+    if (al.size() % 2 == 0) {
+      System.out.println(median(new ArrayList<>((al.subList(0, n/2)))));
+      System.out.println(median(al));
+      System.out.println(median(new ArrayList<>(al.subList(n/2, n))));
+    } else {
+      System.out.println(median(new ArrayList<>((al.subList(0, n/2)))));
+      System.out.println(median(al));
+      System.out.println(median(new ArrayList<>(al.subList(n/2+1, n))));
+    } // end if
+  }
+}
